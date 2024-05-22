1379008a1d7269303d5cdd6dec82820b585318e0
UmerAfzal <123937455+Umer237@users.noreply.github.com>
2024-05-22 17:08:59 +0500

    umer

--- public/data/bannerimg.js
+++ public/data/bannerimg.js
@@ -1,28 +1,30 @@
-export const bannerimgs =
-    [
-        {
-            id: 1,
-            img: 'https://i.pinimg.com/564x/c3/fb/a2/c3fba241dbbb2ff9e256412a25033895.jpg'
-        },
-        {
-            id: 2,
-            img: "https://i.pinimg.com/564x/c1/6a/bd/c16abd9ab16a8fce6633ee769257cdae.jpg"
-        },  {
-            id: 3,
-            img: "https://i.pinimg.com/564x/3d/6a/c4/3d6ac41ff08858e92faa1bec04a0d58b.jpg"
-        },  {
-            id: 4,
-            img: "https://i.pinimg.com/564x/8e/20/0f/8e200fd27609c059db426ea201ac7950.jpg"
-        },
-        {
-            id: 5,
-            img: "https://i.pinimg.com/564x/eb/7e/f7/eb7ef72a19eb7f2f0351a09c5950e5a7.jpg"
-        },  {
-            id: 6,
-            img: "https://i.pinimg.com/564x/ec/1a/2d/ec1a2de7dac2deab8245746da775c7b8.jpg"
-        },  {
-            id: 7,
-            img: "https://i.pinimg.com/564x/4a/60/06/4a6006d87ed676e35ff4966e2d9adc6d.jpg"
-        },
-
-    ]
+export const bannerimgs = [
+  {
+    id: 1,
+    img: "https://i.pinimg.com/564x/c3/fb/a2/c3fba241dbbb2ff9e256412a25033895.jpg",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/564x/c1/6a/bd/c16abd9ab16a8fce6633ee769257cdae.jpg",
+  },
+  {
+    id: 3,
+    img: "https://i.pinimg.com/564x/3d/6a/c4/3d6ac41ff08858e92faa1bec04a0d58b.jpg",
+  },
+  {
+    id: 4,
+    img: "https://i.pinimg.com/564x/8e/20/0f/8e200fd27609c059db426ea201ac7950.jpg",
+  },
+  {
+    id: 5,
+    img: "https://i.pinimg.com/564x/eb/7e/f7/eb7ef72a19eb7f2f0351a09c5950e5a7.jpg",
+  },
+  {
+    id: 6,
+    img: "https://i.pinimg.com/564x/ec/1a/2d/ec1a2de7dac2deab8245746da775c7b8.jpg",
+  },
+  {
+    id: 7,
+    img: "https://i.pinimg.com/564x/4a/60/06/4a6006d87ed676e35ff4966e2d9adc6d.jpg",
+  },
+];
--- src/component/MoviesCategories/index.jsx
+++ src/component/MoviesCategories/index.jsx
@@ -39,7 +39,6 @@ const MoviesCategories = () => {
                 alt=""
               />
             </SwiperSlide>
-
           ))}
 
         </Swiper>
