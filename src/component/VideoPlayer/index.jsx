39d106006e557c5c63dc94a9c5b522ba54cd694a
Your Name <burhanshabbir6189@gmail.com>
2024-05-22 18:52:31 +0500

    burahn

--- src/component/VideoPlayer/index.jsx
+++ src/component/VideoPlayer/index.jsx
@@ -1,20 +1,18 @@
-import { CldVideoPlayer } from "next-cloudinary";
 import React from "react";
+import ReactPlayer from "react-player";
 
 
 const VideoPlayer = () => {
   return (
     <>
       <h1>Video Player</h1>
-      <CldVideoPlayer
-      
-      id="sea-turtle-color"
-      width="1920"
-      height="1080"
-      src="samples/sea-turtle"
-  
+      <ReactPlayer
 
+        url='https://giistyxelor.s3.amazonaws.com/giists/video/video0cP3w019TiZYYcUy22WY.mp4'
+        controls={true}
       />
+
+
     </>
   );
 };
