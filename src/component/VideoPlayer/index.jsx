1af25f84bec6cd94db76e3ca2fac108bd961934e
Your Name <burhanshabbir6189@gmail.com>
2024-05-22 18:34:51 +0500

    burhan

--- src/component/VideoPlayer/index.jsx
+++ src/component/VideoPlayer/index.jsx
@@ -1,3 +1,4 @@
+import { CldVideoPlayer } from "next-cloudinary";
 import React from "react";
 
 
@@ -5,6 +6,15 @@ const VideoPlayer = () => {
   return (
     <>
       <h1>Video Player</h1>
+      <CldVideoPlayer
+      
+      id="sea-turtle-color"
+      width="1920"
+      height="1080"
+      src="samples/sea-turtle"
+  
+
+      />
     </>
   );
 };
